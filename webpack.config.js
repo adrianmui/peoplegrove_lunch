e8e73d7ba1f9dd092b52968a928500d545e6576e
adrian mui <adrian.mui@gmail.com>
2017-08-14 01:13:39 -0700

    added angular 2 with webpack...2 errors left to compile

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const helpers = require('./config/helpers');
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/client/polyfills.ts',
+    'vendor': './src/client/vendor.ts',
+    'main': './src/client/main.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'public', 'js'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: { configFileName: helpers.root('src', 'tsconfig.json') }
+          } , 'angular2-template-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.scss$/,
+        exclude: [path.resolve(__dirname, "node_modules"), path.resolve(__dirname, "/client/*/pages")], 
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader','postcss-loader','sass-loader']
+        })
+      },
+    ]
+  },
+
+  plugins: [
+    // Workaround for angular/angular#11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)@angular/,
+      helpers.root('./src/client'), // location of your src
+      {} // a map of your routes
+    ),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/client/index.html'
+    })
+  ]
+};
+
