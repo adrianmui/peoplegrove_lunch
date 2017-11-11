a431dda35105713d6611a304affb9afd833e18be
adrian mui <adrian.mui@gmail.com>
2017-11-10 23:47:36 -0800

    coding project resumed

--- src/server/pages/routes/index.js
+++ src/server/pages/routes/index.js
@@ -1,19 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
+const { loginRequired } = require("./../../auth/auth");
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.sendFile('./../../../../public/index.html');
+router.get("/", (req, res) => {
+  res.sendFile("./../../../../public/index.html");
 });
-  
-router.get('/landing', (req, res, next) => {
-    console.log(`current user is: ${req.user}`);
-    res.render('index', {user: req.user})
-  
-router.get('/profile', (req, res, next) => {
-  res.render('user/profiles', {user: req.user})
-})
-  
+
+router.get("/landing", (req, res, next) => {
+  console.log(`current user is: ${req.user}`);
+  res.render("index", { user: req.user });
 });
-  
-  
-module.exports = router; 
+
+router.get("/profile", loginRequired, (req, res, next) => {
+  res.render("user/profile", { user: req.user });
+});
+
+module.exports = router;
